c1fd85e39ebd78f849c9e85e25d558fefad78aea
NachtaktiverHalbaffe <LeMonkay.VT&IT@tuanota.com>
2023-07-24 14:46:40 +0000

    First finished implementation of path layer

--- src/sopias4_application/sopias4_application/path_layer.py
+++ src/sopias4_application/sopias4_application/path_layer.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python3
 
+from typing import Tuple
+
 import numpy as np
 import rclpy
-from geometry_msgs.msg import PoseWithCovarianceStamped
+from geometry_msgs.msg import PoseStamped
 from nav2_simple_commander.costmap_2d import PyCostmap2D
 from nav_msgs.msg import Path
+from scipy import ndimage
+from skimage import draw
 from sopias4_framework.nodes.layer_pyplugin import LayerPyPlugin
 from sopias4_framework.tools.ros2 import costmap_tools
 
@@ -30,8 +34,8 @@ class PathLayer(LayerPyPlugin):
                 plugin_name="path_layer",
                 namespace=namespace,
             )
-        self.COST_ROBOTS: np.uint8 = np.uint8(100)
-        self.ROBOT_RADIUS: float = 0.20
+        self.COST_PATH: np.uint8 = np.uint8(100)
+        self.ROBOT_RADIUS: float = 0.25
 
         self.robot_paths: list[Path]
 
@@ -62,6 +66,48 @@ class PathLayer(LayerPyPlugin):
         # Set cost of all pixels in costmap to zero, because only the costs calculated by this layer should be included in the map.
         # In the plugin bridge, all layers get combined so the cleared data doesn't get lost if it is up to date
         costmap.costmap.fill(0)
+        # Convert 1d array costmap data to a 2d grid because it makes things easier and copy it into a new numpy array
+        costmap_grid = costmap_tools.costmap_2_grid(costmap)
+
+        # --- Set costs for path itself which is only 1 pixel wide ---
+        # For each path of each registered robot
+        for path in self.robot_paths:
+            last_node: Tuple[int, int] = costmap_tools.pose_2_costmap(
+                path.poses[0], costmap  # type: ignore
+            )
+            for node in path.poses[1::]:  # type: ignore
+                current_node = costmap_tools.pose_2_costmap(node, costmap)
+                # Basically a Bresenhams line algorithm implementation
+                rr, cc = draw.line(
+                    last_node[0], last_node[1], current_node[0], current_node[1]
+                )
+                costmap_grid[rr, cc] = self.COST_PATH
+                last_node = current_node
+
+        # --- Inflate the path so it is as thick as needed ---
+        # Create a copy of the input array to avoid modifying the original
+        inflated_arr = np.copy(costmap_grid)
+        # Get the dimensions of the array
+        rows, cols = costmap_grid.shape
+        # Convert the distance space into pixel space 
+        inflation_distance_pxl:int = int(self.ROBOT_RADIUS* (1/costmap.getResolution()))
+        if inflation_distance_pxl <1:
+            inflation_distance_pxl = 1
+        
+        # Iterate through the array
+        for row in range(rows):
+            for col in range(cols):
+                if costmap_grid[row, col] == self.COST_PATH:
+                    # Iterate through the neighboring cells within the inflation_distance
+                    for i in range(max(0, row - inflation_distance_pxl), min(rows, row + inflation_distance_pxl + 1)):
+                        for j in range(max(0, col - inflation_distance_pxl), min(cols, col + inflation_distance_pxl+ 1)):
+                            # Check if the cell is within the inflation_distance
+                            if costmap_tools.euclidian_distance_pixel_domain((row, col),( i, j)) <=  inflation_distance_pxl:
+                                # Update the cell value to the target_value
+                                inflated_arr[i, j] = self.COST_PATH
+        
+        # Write grid-based costmap data back into 1d data array of costmap
+        costmap.costmap = costmap_tools.grid_2_costmap(costmap_grid)
 
         return costmap
 
